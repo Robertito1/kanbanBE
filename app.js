9248f017185e1ed10493f60862198976f0baf3fa
Robert <orazurobert@yahoo.com>
2021-08-25 12:45:54 +0100

    create-react-app

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ io.on("connection", (socket) => {
 if (process.env.NODE_ENV === "development") {
   app.use(logger("dev"));
 }
-app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
