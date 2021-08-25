2c631238f6bbbca56a5e6542a35610af26b166e4
Robert <orazurobert@yahoo.com>
2021-08-25 12:43:39 +0100

    create-react-app

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ if (process.env.NODE_ENV === "development") {
   app.use(logger("dev"));
 }
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
